633814df697f3766f9a12aadb7f543112789ef03
liyuqihxc <liyuqihxc@gmail.com>
2018-03-27 17:20:30 +0800

    vue.js

---
+++ XRemote.Controlor.Front/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+
+
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+      spinShow: false,
+      menuData: [],
+      menuTop: [],
+      menuChild: [],
+      btnFlag: [],
+      user: {},
+    },
+    actions: actions,
+    mutations: mutations,
+    modules: {
+    }
+  })
+  
+  export default store
